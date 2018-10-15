c385ca2357839a0ee1286615b6eb96c8c0fe8f5c
tswaehn <sven.ginka@gmail.com>
2018-10-15 21:25:42 +0200

    add: integration of config into phpmpreloaded

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
 
+  <?php  
+  /* some helpers for phpMpReloaded */
+  if (!defined('__PHPMPRELOADED_CLIENT_SWITCHER__')){
+    define('__PHPMPRELOADED_CLIENT_SWITCHER__', '../../lib/ClientSwitcher.php');
+  }
+  if (file_exists(__PHPMPRELOADED_CLIENT_SWITCHER__)){
+    include(__PHPMPRELOADED_CLIENT_SWITCHER__);
+  }
+  ?>
 
 
 </body>
--- mpd/mpdconfig.php
+++ mpd/mpdconfig.php
@@ -3,8 +3,23 @@
  *  mpdconfig.php
  *  Configuration for MPD
  */
- 
-  $host = 'localhost';
-  $mpdPort = 6600;
+
+  /* some helpers for phpMpReloaded */
+  define('__PHPMPRELOADED_MPD_SETTINGS__', '../../config/mpd_config.php' );
+  define('__PHPMPRELOADED_CLIENT_SWITCHER__', '../../../lib/ClientSwitcher.php');
+  if (file_exists( __PHPMPRELOADED_MPD_SETTINGS__ )){
+    include(__PHPMPRELOADED_MPD_SETTINGS__);
+    $host = $mpd_host;
+    $mpdPort = $mpd_port;
+  } else if (file_exists( "../".__PHPMPRELOADED_MPD_SETTINGS__ )){
+    include( "../".__PHPMPRELOADED_MPD_SETTINGS__);
+    $host = $mpd_host;
+    $mpdPort = $mpd_port;
+  } else {
+    $host = "localhost";
+    $mpdPort = 6600;
+    
+  }
+
   $mpdPassword = null;
 ?>
